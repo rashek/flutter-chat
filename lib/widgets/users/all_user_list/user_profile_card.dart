8ab1de43920d1fe28fabdcf3b961e51e4903603a
rashek <rashekhumayun@gmail.com>
2021-03-25 13:53:17 +0600

    request sent and unsent to other user complete

--- lib/widgets/users/all_user_list/user_profile_card.dart
+++ lib/widgets/users/all_user_list/user_profile_card.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 // import 'package:chat/screens/chat_screen.dart';
 
-class UserCard extends StatelessWidget {
+class UserCard extends StatefulWidget {
   final String userId;
   final String username;
   final String userImage;
@@ -20,55 +20,94 @@ class UserCard extends StatelessWidget {
     this.myImage,
     this.isMe,
   );
-  void _sendRequest() async {
-    await Firestore.instance
+
+  @override
+  _UserCardState createState() => _UserCardState();
+}
+
+class _UserCardState extends State<UserCard> {
+  void _sendRequest(check) async {
+    if (check) {
+      await Firestore.instance
+          .collection('user')
+          .document(widget.userId)
+          .collection('requests')
+          .document(widget.myId)
+          .setData({
+        'uid': widget.myId,
+        'username': widget.myName,
+        'image_url': widget.myImage,
+      });
+    } else if (!check) {
+      await Firestore.instance
+          .collection('user')
+          .document(widget.userId)
+          .collection('requests')
+          .document(widget.myId)
+          .delete();
+    }
+    setState(() {});
+  }
+
+  Future<QuerySnapshot> _fetchStatus() async {
+    final ab = await Firestore.instance
         .collection('user')
-        .document(userId)
+        .document(widget.userId)
         .collection('requests')
-        .document(myId)
-        .setData({
-      'uid': myId,
-      'username': username,
-      'image_url': myImage,
-    });
+        .where('uid', isEqualTo: widget.myId)
+        .getDocuments();
+    return ab;
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!isMe)
-      return Card(
-        margin: EdgeInsets.symmetric(
-          horizontal: 5,
-          vertical: 5,
-        ),
-        child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(userImage),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 5),
-                      child: Text(
-                        username,
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.bold),
+    if (!widget.isMe)
+      return FutureBuilder(
+          future: _fetchStatus(),
+          builder: (context, checksnapshot) {
+            if (checksnapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading');
+            }
+            final checkStatus = checksnapshot.data.documents;
+            print(checkStatus.length);
+            return Card(
+              margin: EdgeInsets.symmetric(
+                horizontal: 5,
+                vertical: 5,
+              ),
+              child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(widget.userImage),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 5),
+                            child: Text(
+                              widget.username,
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                OutlinedButton(
-                  onPressed: _sendRequest,
-                  child: Text('Add +'),
-                )
-              ],
-            )),
-        // ),
-      );
+                      OutlinedButton(
+                        onPressed: () {
+                          _sendRequest(checkStatus.length < 1);
+                        },
+                        child: checkStatus.length < 1
+                            ? Text('Add +')
+                            : Text('Added'),
+                      )
+                    ],
+                  )),
+              // ),
+            );
+          });
 
     return Padding(
       padding: const EdgeInsets.all(0.0),
